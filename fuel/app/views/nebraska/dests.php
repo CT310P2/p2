b7729c74727747c3e7227d79f360d7d60c59e41a
ewanlp <ewanlp@gmail.com>
2018-03-29 11:51:02 -0600

    updated

--- fuel/app/views/nebraska/dests.php
+++ fuel/app/views/nebraska/dests.php
@@ -25,7 +25,7 @@ for($i = 0; $i < $length; $i++) {
   <div class="col-6">
     <div class="card-deck">
       <div class="card border-danger text-center">
-        <img class="card-img-top" src=" <?=$image; ?>" alt="Card image cap" height="242" width="162">
+        <img class="card-img-top" src=" ../../img/<?=$image; ?>" alt="Card image cap" height="242" width="162">
         <div class="card-body ">
           <h5 class="card-title"><?=$name; ?></h5>
           <p class="card-text"><?=$overview; ?></p>
@@ -38,55 +38,6 @@ for($i = 0; $i < $length; $i++) {
       </div>
     </div>
   </div>
-  
 <?php } ?>
-
-
 </div>
 
-
-<!--<div class="row">-->
-<!--  <div class="col-6">-->
-<!--    <div class="card-deck">-->
-<!--      <div class="card border-danger">-->
-<!--        <img class="card-img-top" src="../../img/car2.jpg" alt="Card image cap" height="242" width="162">-->
-<!--        <div class="card-body ">-->
-<!--          <h5 class="card-title">Carhenge</h5>-->
-<!--          <p class="card-text">Have you ever wanted to visit the famous Stonehenge but it's too far away? This is the perfect solution!</p>-->
-<!--        </div>-->
-<!--        <div class="card-footer">-->
-<!--          <small class="text-muted"><a href="--><?//=Uri::create('index.php/nebraska/carhenge'); ?><!--" class="btn btn-danger">Learn More</a></small>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-<!--  <div class="col-6">-->
-<!--    <div class="card-deck">-->
-<!--      <div class="card border-danger">-->
-<!--        <img class="card-img-top" src="../../img/zoo.jpg" alt="Card image cap" height="242" width="162">-->
-<!--        <div class="card-body">-->
-<!--          <h5 class="card-title">Henry Doorly Zoo and Aquarium</h5>-->
-<!--          <p class="card-text">One of the world's best zoo's with some of the largest, wildest, exhibits!</p>-->
-<!--        </div>-->
-<!--        <div class="card-footer">-->
-<!--          <small class="text-muted"><a href="--><?//=Uri::create('index.php/nebraska/zooAqua'); ?><!--" class="btn btn-danger">Learn More</a></small>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-<!---->
-<!--  <div class="col-6">-->
-<!--    <div class="card-deck">-->
-<!--      <div class="card border-danger">-->
-<!--        <img class="card-img-top" src="../../img/chimney.jpg" alt="Card image cap" height="242" width="162">-->
-<!--        <div class="card-body">-->
-<!--          <h5 class="card-title">Chimney Rock</h5>-->
-<!--          <p class="card-text">Beautiful landscape also exists in Nebraska, take a look!</p>-->
-<!--        </div>-->
-<!--        <div class="card-footer">-->
-<!--          <small class="text-muted"><a href="--><?//=Uri::create('index.php/nebraska/chimney'); ?><!--" class="btn btn-danger">Learn More</a></small>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-<!--</div>-->
