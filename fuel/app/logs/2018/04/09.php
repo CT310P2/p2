6527144e80696db356b31745470ed8b1b2862313
Sabrina White <sabrinaw@london.cs.colostate.edu>
2018-04-09 14:22:48 -0600

    photo upload working' ;

---
+++ fuel/app/logs/2018/04/09.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2018-04-09 14:17:19 --> Notice - Trying to get property of non-object in /s/bach/e/under/sabrinaw/fuel/app/views/nebraska/layoutfull.php on line 11
