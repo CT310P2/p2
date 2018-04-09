bf8e765835c0479956f487e8c9dc366d6fad33f0
ewanlp <ewanlp@gmail.com>
2018-04-09 14:30:54 -0600

    yupp

--- fuel/app/views/nebraska/success.php
+++
@@ -1,5 +0,0 @@
-<?php if($status === 'success') {
-  header('location: http://www.cs.colostate.edu/~ewanlp/ct310/index.php/nebraska/index');
-}else {
-  header('location: http://www.cs.colostate.edu/~ewanlp/ct310/index.php/nebraska/loginError');
-} ?>
